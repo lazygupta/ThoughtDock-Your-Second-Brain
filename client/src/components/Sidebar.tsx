0a6cc0872f1e6b03826718f7993511a5844e5031
Ankit Gupta <guptashirley02@gmail.com>
2024-12-02 05:31:22 +0530

    Added Logout functionality

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import DocumentIcon from "../icons/DocumentIcon";
 import LinkIcon from "../icons/LinkIcon";
 import { Logo } from "../icons/Logo";
@@ -8,7 +9,17 @@ import { SideBarItem } from "./SidebarItem";
 
 
 export function Sidebar() {
+    const navigate = useNavigate();
+
+    function logout () {
+        localStorage.removeItem("authorization")
+        alert("You are logged out successfully!");
+
+        navigate("/");
+    }
+
     return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 ">
+        
         <div className="flex text-2xl px-2 pt-4 gap-2 items-center font-bold">
             <div className="text-purple-500 pl-2">
                 <Logo />
@@ -22,6 +33,12 @@ export function Sidebar() {
             <SideBarItem text="Links" icon={<LinkIcon size="lg"/>}/>
             <SideBarItem text="Tags" icon={<TagIcon size="lg"/>}/>
         </div>
-        
+
+        <div className="mt-auto p-4">
+                <button className="w-full text-left px-4 py-2 rounded-md bg-red-500 text-white hover:bg-red-600" onClick={logout}>
+                    LogOut
+                </button>
+            </div>
+       
     </div>
 }
